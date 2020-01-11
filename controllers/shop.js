e75eda845f14704675a84580a526a3a867c24590
leoncross <leon@lscross.co.uk>
2020-01-11 14:32:30 +0000

    controller passes body to shopModel.buy

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,9 @@
+const shopModel = require('../models/shop');
+
 exports.buy = (req, res) => {
   const { items, currency } = req.body;
+
+  shopModel.buy(items, currency);
 
   res.status(200);
   res.json({ items, currency });
--- tests/unit/controllers/shop.test.js
+++ tests/unit/controllers/shop.test.js
@@ -5,10 +5,16 @@ const chai = require('chai').use(sinonChai);
 const { expect } = chai;
 
 const shop = require('../../../controllers/shop');
+const shopModel = require('../../../models/shop');
 
 describe('Shop controller', () => {
   let res;
   let req;
+
+  let items;
+  let currency;
+
+  let shopModelBuyStub;
 
   beforeEach(() => {
     req = {};
@@ -16,19 +22,38 @@ describe('Shop controller', () => {
 
     res.json = sinon.stub().returns(res);
     res.status = sinon.stub().returns(res);
+
+    shopModelBuyStub = sinon.stub(shopModel, 'buy');
+  });
+
+  afterEach(() => {
+    sinon.restore();
   });
 
   describe('#buy', () => {
-
     it('returns in json whatever is passed to the request body', () => {
       req.body = {
-        items: ['apple', 'apple', 'milk'],
-        currency: 'EUR',
+        items,
+        currency,
       };
 
       shop.buy(req, res);
 
       expect(res.json).calledOnceWith(req.body);
+    });
+
+    it('calls buy on shopModel', () => {
+      items = ['apple', 'milk'];
+      currency = 'GBP';
+
+      req.body = {
+        items,
+        currency,
+      };
+
+      shop.buy(req, res);
+
+      expect(shopModelBuyStub).calledOnceWith(items, currency);
     });
   });
 });

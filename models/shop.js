517aa65e94704fc675ecff2d9273b4d25f60db92
leoncross <leon@lscross.co.uk>
2020-01-11 15:16:10 +0000

    implemented order handling

--- models/shop.js
+++ models/shop.js
@@ -1,11 +1,50 @@
-exports.buy = (items, currency) => {
+const catalog = {
+  apple: {
+    price: 1,
+    offer: {
+      type: 'percentage',
+      amountOff: 0.1,
+      minPurchase: 1,
+      name: '10% off apples',
+    },
+  },
+  milk: {
+    price: 1.15,
+    offer: {
+      type: 'fixed',
+      amountOff: 0.5,
+      minPurchase: 3,
+      name: 'buy 3 milk, get 50c off',
+    },
+  },
+  bread: { price: 0.8 },
+  soup: { price: 0.65 },
+};
+
+const handleItems = (items) => {
   const order = {
     subtotal: 0,
-    discounts: [],
     discountAmt: 0,
-    total: 0,
-    currency: 0,
+    discounts: [],
   };
+
+  items.forEach((item) => {
+    if (!Object.prototype.hasOwnProperty.call(order, item)) {
+      order[item] = { count: 0, price: 0 };
+    }
+    order[item].count += 1;
+    order[item].price += catalog[item].price;
+    order.subtotal += catalog[item].price;
+  });
+
+  return order;
+};
+
+exports.buy = (items, currency) => {
+  const order = handleItems(items);
+
+  order.total = 0;
+  order.currency = currency;
 
   return order;
 };
--- tests/unit/models/shop.test.js
+++ tests/unit/models/shop.test.js
@@ -18,5 +18,41 @@ describe('Shop Model', () => {
       expect(order).to.have.deep.property('total');
       expect(order).to.have.deep.property('currency');
     });
+    context('item handling', () => {
+      it('iterates through items against catalog and updates order details', () => {
+        const items = ['apple', 'soup', 'milk'];
+        const currency = 'GBP';
+
+        const order = shop.buy(items, currency);
+
+        expect(order).to.deep.equal({
+          subtotal: 2.8,
+          discountAmt: 0,
+          discounts: [],
+          apple: { count: 1, price: 1 },
+          milk: { count: 1, price: 1.15 },
+          soup: { count: 1, price: 0.65 },
+          total: 0,
+          currency: 'GBP',
+        });
+      });
+      it('handles multiple of the same item without duplicating object', () => {
+        const items = ['apple', 'apple', 'soup', 'milk'];
+        const currency = 'GBP';
+
+        const order = shop.buy(items, currency);
+
+        expect(order).to.deep.equal({
+          subtotal: 3.8,
+          discountAmt: 0,
+          discounts: [],
+          apple: { count: 2, price: 2 },
+          milk: { count: 1, price: 1.15 },
+          soup: { count: 1, price: 0.65 },
+          total: 0,
+          currency: 'GBP',
+        });
+      });
+    });
   });
 });

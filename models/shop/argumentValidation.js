ec05e332eca3a90d2a3b4e4e5a668e346eff2edf
leoncross <leon@lscross.co.uk>
2020-01-12 11:02:25 +0000

    refactored and extracted argument validation

--- models/shop/argumentValidation.js
+++ models/shop/argumentValidation.js
@@ -1,34 +1,52 @@
 const { currencies, catalog } = require('./config');
 
-const argumentValidation = (data) => {
-  const errorMsg = {
-    error: {
-      errorMessage: undefined,
-      errorType: 'BAD_REQUEST',
-      httpStatus: 422,
-    },
-  };
+const errorMsg = {
+  error: {
+    errorMessage: undefined,
+    errorType: 'BAD_REQUEST',
+    httpStatus: 422,
+  },
+};
 
+const checkItemIsDefined = (data) => {
   if (!data.items) {
     errorMsg.error.errorMessage = 'no items provided in request';
     throw errorMsg;
   }
+};
 
+const checkCurrencyIsDefined = (data) => {
   if (!data.currency) {
     errorMsg.error.errorMessage = 'no currency provided in request';
     throw errorMsg;
   }
+};
 
+const checkItemsAreInCatalog = (data) => {
   data.items.forEach((item) => {
     if (!catalog[item]) {
       errorMsg.error.errorMessage = `item: "${item}" is not a valid item`;
       throw errorMsg;
     }
   });
+};
 
+const checkCurrencyIsAccepted = (data) => {
   if (!Object.prototype.hasOwnProperty.call(currencies, data.currency)) {
     errorMsg.error.errorMessage = `currency: "${data.currency}" is not an accepted currency`;
     throw errorMsg;
+  }
+};
+
+const argumentValidation = (data) => {
+  try {
+    checkItemIsDefined(data);
+    checkCurrencyIsDefined(data);
+
+    checkItemsAreInCatalog(data);
+    checkCurrencyIsAccepted(data);
+  } catch (err) {
+    throw err;
   }
 };
 
